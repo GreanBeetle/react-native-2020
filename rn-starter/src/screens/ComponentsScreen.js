0b02a0c501186ad6f51d671baa0de5b003bb5720
John Rykken <john.rykken@gmail.com>
2020-10-29 14:34:28 -0700

    go over beginner stuff ...

---
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const ComponentsScreen = () => {
+  return <Text style={styles.text}>components screen</Text>
+}
+
+export default ComponentsScreen
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 42
+  }
+})
