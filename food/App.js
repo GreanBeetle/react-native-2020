d15c41a6222cbc09b53b1f550ef598921b16e978
John Rykken <john.rykken@gmail.com>
2020-11-05 12:27:59 -0800

    add gestureHandlerRootHOC to food/src/app.js

--- food/App.js
+++ food/App.js
@@ -1,6 +1,8 @@
+import 'react-native-gesture-handler'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import STYLES from './src/styles'
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
 const App = () => {
   return (
@@ -12,4 +14,4 @@ const App = () => {
 
 const styles = StyleSheet.create({ })
 
-export default App 
+export default gestureHandlerRootHOC(App) 
