21892fbbc29f8b2dbff25a16eac3f5bffb2293bf
John Rykken <john.rykken@gmail.com>
2020-11-12 11:11:49 -0800

    successfully make and parse getBusinessAPI call

--- food/src/screens/ResultShowScreen.js
+++ food/src/screens/ResultShowScreen.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import STYLES from '../styles'
+import yelp from '../api/yelp'
 
 const ResultShowScreen = props => {
   const { id } = props.route.params
-  console.log('id', id)
+  const [results, setResults] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const getBusinessAPI = async (ID) => {
+    try {
+      const response = await yelp.get(`${ID}`)
+      console.log('getBusinessAPI response', response.data)
+      setResults(response.data)
+    } catch (error) {
+      setErrorMessage(error.messsage)
+    }
+  }
+
+  useEffect( () => {
+    getBusinessAPI(id)
+  }, [])
+
   return (
     <View>
-      <Text>Result Show Screen</Text>
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>{results ? results.alias : 'no results yet'}</Text>
     </View>
   )
 }
