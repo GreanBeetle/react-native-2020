c4f13f357ea8e77352ae769ba2b69e45cf3519cc
John Rykken <john.rykken@gmail.com>
2020-11-11 19:19:13 -0800

    add reminder to simplify the route params

--- food/src/screens/ResultShowScreen.js
+++ food/src/screens/ResultShowScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native'
 import STYLES from '../styles'
 
 const ResultShowScreen = props => {
-  const { id } = props.route.params
+  const { id } = props.route.params // PASS ONLY ID! there's no need to destructure off of props 
   console.log('ID on Result SHOW', id) // REMOVE 
   return (
     <View>
