f7861d3545af2dd1ef0d9f6fc3c99fa68304fe43
John Rykken <john.rykken@gmail.com>
2020-11-19 14:37:28 -0800

    in app.js wrap <App/> with <BlogProvider/>

--- blog/App.js
+++ blog/App.js
@@ -1,8 +1,13 @@
 import 'react-native-gesture-handler'
 import React from 'react'
 import Navigation from './src/navigation'
+import { BlogProvider } from './src/context/BlogContext'
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
-const App = () => <Navigation />
+const App = () => (
+  <BlogProvider>
+    <Navigation />
+  </BlogProvider>
+)
 
 export default gestureHandlerRootHOC(App) 
